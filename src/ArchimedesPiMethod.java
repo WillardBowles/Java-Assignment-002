e0f98ef9d91a5dadd516beaca1aa88defac64ef2
WillardBowles <relic.dex@gmail.com>
2023-01-29 21:21:52 -0800

    Assignment2 done WillardBowles

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,24 @@
-import java.util.Scanner;
-
-public class ArchimedesPiMethod {
+/**
+ * @author Trevor Hartman
+ * @author Willard Bowles
+ * @since Version 1.0
+ *
+ *
+ *
+ */
+public class ArchimedesPiMethod{
     public static void main(String[] args) {
+
+        double sides = 25.0;
+        double angleB = 360.0/sides;
+        double angleA = angleB/2 ;
+        double radianA = Math.toRadians(angleA);
+        double sinA = Math.sin(radianA);
+        double base = sinA * 2;
+        double length = sides * base;
+        double Pi = length/2;
+        //Hopefully this is satisfactory. I am starting to get a hang of some of the basic concepts.
+        System.out.println(Pi);
 
     }
 }
